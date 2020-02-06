631d1f809f85b4b2b8155f3e314337bdebf1e832
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-02-06 21:43:22 +0100

    Fixed everything

--- src/Lab1.java
+++ src/Lab1.java
@@ -2,17 +2,33 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
+import java.util.Set;
 import java.util.concurrent.Semaphore;
 
 import TSim.*;
 
 public class Lab1 {
   private Map<Position, CrossingSensor> crossingSensors = new HashMap<>();
-  private Map<Position, Void> endSensors = new HashMap<>();
+  private Set<Position> endSensors = new HashSet<>();
   private List<Semaphore> semaphores = new ArrayList<>();
+
+  private class SensorConfig {
+    private Region region;
+    private Direction direction;
+
+    public SensorConfig(Region region, Direction direction) {
+      this.region = region;
+      this.direction = direction;
+    }
+  }
+
+  private enum Direction {
+    LEFT, RIGHT
+  }
 
   private enum Region {
     A(0), B(1), C(2), D(3), E(4), F(5), G(6), H(7), MAX(8);
@@ -25,19 +41,43 @@ public class Lab1 {
   }
 
   private class Position {
-    public int x;
-    public int y;
+    public final int x;
+    public final int y;
 
     public Position(int x, int y) {
       this.x = x;
       this.y = y;
     }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o)
+        return true;
+      if (o == null || getClass() != o.getClass())
+        return false;
+      Position position = (Position) o;
+      if (x != position.x)
+        return false;
+      if (y != position.y)
+        return false;
+      return true;
+    }
+
+    @Override
+    public int hashCode() {
+      int hash = 17;
+      hash = ((hash + x) << 5) - (hash + x);
+      hash = ((hash + y) << 5) - (hash + y);
+      return hash;
+    }
   }
 
   private class CrossingSensor {
-    List<Region> regions;
+    List<SensorConfig> regions;
+    Position switchPosition;
 
-    public CrossingSensor(List<Region> regions) {
+    public CrossingSensor(Position switchPosition, List<SensorConfig> regions) {
+      this.switchPosition = switchPosition;
       this.regions = regions;
     }
   }
@@ -56,34 +96,57 @@ public class Lab1 {
 
     @Override
     public void run() {
-      // try {
-      // tsi.setSpeed(id, speed);
-      // SensorEvent event = tsi.getSensor(id);
-      // if (event.getStatus() == SensorEvent.ACTIVE) {
-      // if (event.getXpos() == 16 && event.getYpos() == 7) {
-      // if (semaphoreC.tryAcquire()) {
-      // tsi.setSwitch(17, 7, TSimInterface.SWITCH_RIGHT);
-      // semaphoreA.release();
-      // } else {
-      // tsi.setSpeed(id, 0);
-      // while (!semaphoreC.tryAcquire(10, TimeUnit.SECONDS))
-      // ;
-      // tsi.setSwitch(17, 7, TSimInterface.SWITCH_RIGHT);
-      // semaphoreA.release();
-      // tsi.setSpeed(id, speed);
-      // }
-      // }
-      // }
-      // while (true) {
-      // SensorEvent event = tsi.getSensor(id);
-      // if (event.getStatus() == SensorEvent.ACTIVE) {
-      // tsi.setSpeed(id, 0);
-      // }
-      // System.out.println("Status " + (event.getStatus() == SensorEvent.ACTIVE));
-      // }
-      // } catch (InterruptedException | CommandException e) {
-      // e.printStackTrace();
-      // }
+      try {
+        tsi.setSpeed(id, speed);
+        while (true) {
+          SensorEvent event = tsi.getSensor(id);
+          if (event.getStatus() == SensorEvent.INACTIVE) {
+            continue;
+          }
+          Position position = new Position(event.getXpos(), event.getYpos());
+          CrossingSensor crossingSensor = crossingSensors.get(position);
+          if (crossingSensor != null) {
+            if (crossingSensor.regions.size() > 1) {
+              if (regions.size() > 1) {
+                semaphores.get(regions.poll().value).release();
+              } else {
+                for (SensorConfig region : crossingSensor.regions) {
+                  Semaphore semaphore = semaphores.get(region.region.value);
+                  if (semaphore.tryAcquire()) {
+                    regions.add(region.region);
+                    tsi.setSwitch(crossingSensor.switchPosition.x, crossingSensor.switchPosition.y,
+                        region.direction == Direction.RIGHT ? TSimInterface.SWITCH_RIGHT : TSimInterface.SWITCH_LEFT);
+                    break;
+                  }
+                }
+              }
+            } else {
+              if (regions.size() > 1) {
+                semaphores.get(regions.poll().value).release();
+              } else {
+                SensorConfig region = crossingSensor.regions.get(0);
+                Semaphore semaphore = semaphores.get(region.region.value);
+                if (!semaphore.tryAcquire()) {
+                  tsi.setSpeed(id, 0);
+                  semaphore.acquire();
+                }
+                tsi.setSpeed(id, speed);
+                regions.add(region.region);
+                tsi.setSwitch(crossingSensor.switchPosition.x, crossingSensor.switchPosition.y,
+                    region.direction == Direction.RIGHT ? TSimInterface.SWITCH_RIGHT : TSimInterface.SWITCH_LEFT);
+              }
+            }
+          }
+          if (endSensors.contains(position)) {
+            tsi.setSpeed(id, 0);
+            Thread.sleep(1000 + 20 * Math.abs(speed), 0);
+            speed *= -1;
+            tsi.setSpeed(id, speed);
+          }
+        }
+      } catch (InterruptedException | CommandException e) {
+        e.printStackTrace();
+      }
     }
   }
 
@@ -94,31 +157,38 @@ public class Lab1 {
         semaphores.add(new Semaphore(1));
       }
       semaphores.get(Region.A.value).acquire();
-      semaphores.get(Region.C.value).acquire();
       semaphores.get(Region.G.value).acquire();
 
       // Initialise sensors
-      crossingSensors.put(new Position(2, 11),
-          new CrossingSensor(new ArrayList<Region>(Arrays.asList(Region.G, Region.H))));
-      crossingSensors.put(new Position(3, 9),
-          new CrossingSensor(new ArrayList<Region>(Arrays.asList(Region.D, Region.E))));
-      crossingSensors.put(new Position(3, 12), new CrossingSensor(new ArrayList<Region>(Arrays.asList(Region.F))));
-      crossingSensors.put(new Position(4, 10), new CrossingSensor(new ArrayList<Region>(Arrays.asList(Region.F))));
-      crossingSensors.put(new Position(4, 11), new CrossingSensor(new ArrayList<Region>(Arrays.asList(Region.F))));
-      crossingSensors.put(new Position(5, 9), new CrossingSensor(new ArrayList<Region>(Arrays.asList(Region.F))));
-      crossingSensors.put(new Position(14, 9), new CrossingSensor(new ArrayList<Region>(Arrays.asList(Region.C))));
-      crossingSensors.put(new Position(15, 10), new CrossingSensor(new ArrayList<Region>(Arrays.asList(Region.C))));
-      crossingSensors.put(new Position(16, 7), new CrossingSensor(new ArrayList<Region>(Arrays.asList(Region.C))));
-      crossingSensors.put(new Position(16, 9),
-          new CrossingSensor(new ArrayList<Region>(Arrays.asList(Region.D, Region.E))));
-      crossingSensors.put(new Position(17, 8), new CrossingSensor(new ArrayList<Region>(Arrays.asList(Region.C))));
-      crossingSensors.put(new Position(18, 7),
-          new CrossingSensor(new ArrayList<Region>(Arrays.asList(Region.A, Region.B))));
+      crossingSensors.put(new Position(1, 10), new CrossingSensor(new Position(3, 11), new ArrayList<SensorConfig>(
+          Arrays.asList(new SensorConfig(Region.G, Direction.LEFT), new SensorConfig(Region.H, Direction.RIGHT)))));
+      crossingSensors.put(new Position(1, 9), new CrossingSensor(new Position(4, 9), new ArrayList<SensorConfig>(
+          Arrays.asList(new SensorConfig(Region.D, Direction.LEFT), new SensorConfig(Region.E, Direction.RIGHT)))));
+      crossingSensors.put(new Position(4, 13), new CrossingSensor(new Position(3, 11),
+          new ArrayList<SensorConfig>(Arrays.asList(new SensorConfig(Region.F, Direction.RIGHT)))));
+      crossingSensors.put(new Position(6, 10), new CrossingSensor(new Position(4, 9),
+          new ArrayList<SensorConfig>(Arrays.asList(new SensorConfig(Region.F, Direction.RIGHT)))));
+      crossingSensors.put(new Position(6, 11), new CrossingSensor(new Position(3, 11),
+          new ArrayList<SensorConfig>(Arrays.asList(new SensorConfig(Region.F, Direction.LEFT)))));
+      crossingSensors.put(new Position(7, 9), new CrossingSensor(new Position(4, 9),
+          new ArrayList<SensorConfig>(Arrays.asList(new SensorConfig(Region.F, Direction.LEFT)))));
+      crossingSensors.put(new Position(12, 9), new CrossingSensor(new Position(15, 9),
+          new ArrayList<SensorConfig>(Arrays.asList(new SensorConfig(Region.C, Direction.RIGHT)))));
+      crossingSensors.put(new Position(13, 10), new CrossingSensor(new Position(15, 9),
+          new ArrayList<SensorConfig>(Arrays.asList(new SensorConfig(Region.C, Direction.LEFT)))));
+      crossingSensors.put(new Position(14, 7), new CrossingSensor(new Position(17, 7),
+          new ArrayList<SensorConfig>(Arrays.asList(new SensorConfig(Region.C, Direction.RIGHT)))));
+      crossingSensors.put(new Position(15, 8), new CrossingSensor(new Position(17, 7),
+          new ArrayList<SensorConfig>(Arrays.asList(new SensorConfig(Region.C, Direction.LEFT)))));
+      crossingSensors.put(new Position(18, 9), new CrossingSensor(new Position(15, 9), new ArrayList<SensorConfig>(
+          Arrays.asList(new SensorConfig(Region.D, Direction.RIGHT), new SensorConfig(Region.E, Direction.LEFT)))));
+      crossingSensors.put(new Position(19, 8), new CrossingSensor(new Position(17, 7), new ArrayList<SensorConfig>(
+          Arrays.asList(new SensorConfig(Region.A, Direction.RIGHT), new SensorConfig(Region.B, Direction.LEFT)))));
 
-      endSensors.put(new Position(17, 3), null);
-      endSensors.put(new Position(17, 5), null);
-      endSensors.put(new Position(17, 11), null);
-      endSensors.put(new Position(17, 13), null);
+      endSensors.add(new Position(15, 3));
+      endSensors.add(new Position(15, 5));
+      endSensors.add(new Position(15, 11));
+      endSensors.add(new Position(15, 13));
 
       new Thread(new Train(1, speed1, Region.A)).start();
       new Thread(new Train(2, speed2, Region.G)).start();
